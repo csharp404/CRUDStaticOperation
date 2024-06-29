77543ec54d04a4742cd1252d098db4ca9139577c
Joe404S <newyorkstoreq@gmail.com>
2024-06-29 03:26:12 +0300

    ComitAll

--- school/Models/StudentModel.cs
+++ school/Models/StudentModel.cs
@@ -1,9 +1,11 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 namespace school.Models
 {
     public class StudentModel
     {
+        [Display(Name="StudentID")]
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
